c14c64dc4ed0af808ede857aec996e279b2e7fe0
Rahul Srivastava <rahuls@zeta.tech>
2018-02-18 18:43:07 +0530

    selectbox

--- src/AutoComplete.js
+++ src/AutoComplete.js
@@ -1,18 +1,24 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
+//import PropTypes from "prop-types";
 import "./css/autoComplete.css";
 
 export default class AutoComplete extends Component {
-  static propTypes = { prop: PropTypes };
+  //static propTypes = { prop: PropTypes };
 
   constructor(props) {
     super(props);
-    this.state = { showOptions: false, selected: { id: 0, name: "Select" } };
+    this.state = {
+      showOptions: false,
+      selected: { id: 0, name: "Select" },
+      searchBox: false,
+      filter: '',
+    };
   }
 
   toggleOptions = evt => {
     this.setState({
-      showOptions: !this.state.showOptions
+      showOptions: !this.state.showOptions,
+      searchBox: !this.state.searchBox
     });
   };
 
@@ -20,28 +26,57 @@ export default class AutoComplete extends Component {
     const currentTarget = evt.currentTarget;
     this.setState({
       showOptions: false,
-      selected: { id: currentTarget.id, name: currentTarget.textContent }
+      selected: {
+        id: currentTarget.id,
+        name: currentTarget.textContent
+      }
     });
   };
 
-  render() {
-    let listEle = this.props.list.map(val => {
-      return (
-        <li key={val.id} id={val.id} onClick={this.selectItem}>
+  searchItem = evt => {
+    evt.stopPropagation();
+  };
+
+  search = evt => {
+    this.setState({ filter: evt.currentTarget.value })
+  }
+
+  searchBox = () => {
+    return <input onClick={this.searchItem} onKeyUp={this.search} className="search-box" type="text" />;
+  };
+
+  selectedItem = () => {
+    return <span onClick={this.searchItem}>{this.state.selected.name}</span>;
+  };
+
+
+  optionsList = () => {
+    const filteredList = this.props.list.filter((val) => val.name.includes(this.state.filter));
+    const optionsList = filteredList.map(val => {
+      return <li key={val.id} id={val.id} onClick={this.selectItem}>
           {val.name}
-        </li>
-      );
+        </li>;
     });
-  let showlistEle = this.state.showOptions ? (<ul>{listEle}</ul>) : null;
-    return <div className="auto-complete" onBlur={this.toggleOptions}>
-        <span onClick={this.toggleOptions}>
-          {this.state.selected.name}
+    const optionsListWrapper = this.state.showOptions ? (
+      <ul>{optionsList}</ul>
+    ) : null;
+    return optionsListWrapper;
+  };
+
+  render() {
+    const searchBox = this.state.searchBox
+      ? this.searchBox()
+      : this.selectedItem();
+    return (
+      <div className="auto-complete" onClick={this.toggleOptions}>
+        <div className="selected">
+          {searchBox}
           <svg width="12" height="12">
             <path d="M0 0 L5 6 L10 0" />
           </svg>
-        </span>
-        <input type="text" />
-        {showlistEle}
-      </div>;
+        </div>
+        {this.optionsList()}
+      </div>
+    );
   }
 }

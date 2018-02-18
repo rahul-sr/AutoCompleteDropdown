f690503117e07808d509e6fcf73f0437ac4803e5
Rahul Srivastava <rahuls@zeta.tech>
2018-02-18 15:59:42 +0530

    selectbox

--- src/AutoComplete.js
+++ src/AutoComplete.js
@@ -1,22 +1,49 @@
-import React from "react";
-import "../css/autoComplete.css";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "./css/autoComplete.css";
 
-function show() {
- 
+export default class AutoComplete extends Component {
+  static propTypes = { prop: PropTypes };
+
+  constructor(props) {
+    super(props);
+    this.state = { showOptions: false, selected: { id: 0, name: "Select" } };
+  }
+
+  toggleOptions = evt => {
+    this.setState({
+      showOptions: !this.state.showOptions
+    });
+  };
+
+  selectItem = evt => {
+    const currentTarget = evt.currentTarget;
+    this.setState({
+      showOptions: false,
+      selected: { id: currentTarget.id, name: currentTarget.textContent }
+    });
+  };
+
+  render() {
+    let listEle = this.props.list.map(val => {
+      return (
+        <li key={val.id} id={val.id} onClick={this.selectItem}>
+          {val.name}
+        </li>
+      );
+    });
+    let showlistEle = this.state.showOptions ? listEle : null;
+    return (
+      <div className="auto-complete" onBlur={this.toggleOptions}>
+        <span onClick={this.toggleOptions}>
+          {this.state.selected.name}
+          <svg width="12" height="12">
+            <path d="M0 0 L5 6 L10 0" />
+          </svg>
+        </span>
+        <input type="text" />
+        <ul>{showlistEle}</ul>
+      </div>
+    );
+  }
 }
-
-export default ({ list }) => {
-  let listEle = list.map(val => {
-    return <li>{val.name}</li>;
-  });
-  return (
-    <div className="auto-complete">
-      <span onClick={show}>Selected</span>
-      <svg width="12" height="12">
-        <path d="M0 0 L5 6 L10 0"></path>
-      </svg>
-      <input type="text" />
-      <ul>{listEle}</ul>
-    </div>
-  );
-};
--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import AutoComplete from './AutoComplete';
+import React from "react";
+import { render } from "react-dom";
+import AutoComplete from "./AutoComplete";
 
 const styles = {
-  fontFamily: 'sans-serif',
-  textAlign: 'center',
+  fontFamily: "sans-serif",
+  textAlign: "center"
 };
 
-const data= [{id:1,name:'test1'},{id:2, name:'test2'}, 
-  { id: 3, name: 'test3' }]
+const data = [
+  { id: 1, name: "test1" },
+  { id: 2, name: "test2" },
+  { id: 3, name: "test3" }
+];
 
 const App = () => (
   <div style={styles}>
-    <Hello name="CodeSandbox" />
-    <h2>Start editing to see some magic happen {'\u2728'}</h2>
+    <h2>AutoComplete Selectbox</h2>
     <AutoComplete list={data} />
   </div>
 );
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
